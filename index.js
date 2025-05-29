1e62ef783a36714a2622205ed0bc0f5614b169af
111Makss111 <777docmax777@gmail.com>
2025-05-29 21:35:50 +0200

    і

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
+const token = process.env.BOT_TOKEN;
+const url = process.env.RENDER_EXTERNAL_URL; // це Render автоматично дає тобі у Settings → Environment
+
+const bot = new TelegramBot(token, {
+  webHook: {
+    port: process.env.PORT || 3000,
+  },
+});
+
+bot.setWebHook(`${url}/bot${token}`);
+
+const app = express();
+app.use(express.json());
+
+// Обробка запитів від Telegram
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Перевірка живості
+app.get("/", (req, res) => {
+  res.send("Bot is running on webhook!");
+});
+
+// Підключення обробників
 const { startCommand } = require("./keyboard/mainMenu.js");
 const handleRecord = require("./handlers/handleRecord.js");
 const handleMyAccount = require("./handlers/handleMyAccount.js");
@@ -11,32 +37,15 @@ const cleanOldRecords = require("./utils/cleanOldRecords");
 const { showTimeSelector } = require("./utils/timeSelector.js");
 const handleReminders = require("./handlers/handleReminders.js");
 
-// Запускаємо функції
 startCommand(bot);
 handleRecord(bot);
 createInlineCalendar(bot);
 showTimeSelector(bot);
 handleReminders(bot);
 cleanOldRecords();
-// Ловимо помилки
-bot.on("polling_error", (error) => {
-  console.error(
-    "Polling error:",
-    error.code,
-    error.response?.body || error.message
-  );
-  if (error.code === "ETELEGRAM" && error.response?.error_code === 409) {
-    console.error("Another instance is running. Exiting process.");
-    process.exit(1);
-  }
-});
-const PORT = process.env.PORT || 3000;
-http
-  .createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Bot is running");
-  })
-  .listen(PORT);
+
+console.log("Express server started, bot webhook is active.");
+
 // bot.on("polling_error", (error) => {
 //   console.error(
 //     "Polling error:",
